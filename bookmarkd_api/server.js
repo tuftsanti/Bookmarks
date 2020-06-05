626f69de2f8d0ceec8263d760824a843899388af
Ian Galleher <ian.galleher@gmail.com>
2020-06-05 13:49:47 -0500

    controllers/routes added to server.js backend complete except for auth

--- bookmarkd_api/server.js
+++ bookmarkd_api/server.js
@@ -1,13 +1,10 @@
 // DEPENDENCIES
 const express = require('express');
 const app = express();
-
 const mongoose = require('mongoose');
-
 
 //GLOBALS
 const PORT = process.env.PORT || 3000;
-const bookmarksController = require('./controllers/bookmarks.js');
 const MONGOURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/bookmarks';
 
 //DB CONNECTION
@@ -16,16 +13,13 @@ mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.once('open', () => console.log('connected to mongoose...'));
 
-// Middleware
+// MIDDLEWARE
 app.use(express.json());
 app.use('/bookmarks', bookmarksController);
 
-
-app.get('/', (req,res)=>{
-    res.send('Hello World')
-})
-
-
+// CONTROLLERS/ROUTES
+const bookmarksController = require('./controllers/bookmarks.js');
+app.use('/bookmarks', bookmarksController);
 
 
 app.listen(PORT, ()=>{
