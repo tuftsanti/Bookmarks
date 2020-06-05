1752a1181554677b4938b6c151c94134a66d6db8
Ian Galleher <ian.galleher@gmail.com>
2020-06-05 11:51:09 -0500

    created controllers, connected to mongoDB

--- bookmarkd_api/server.js
+++ bookmarkd_api/server.js
@@ -1,10 +1,23 @@
+// DEPENDENCIES
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000
 
+const mongoose = require('mongoose');
+
+
+//GLOBALS
+const PORT = process.env.PORT || 3000;
+const bookmarksController = require('./controllers/bookmarks.js');
+const MONGOURL = process.env.MONGODB_URL || 'mongodb://localhost:27017/bookmarks';
+
+//DB CONNECTION
+mongoose.connection.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
+mongoose.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => console.log('connected to mongoose...'));
 
 // Middleware
-
+app.use('/bookmarks', bookmarksController);
 
 
 app.get('/', (req,res)=>{
@@ -15,5 +28,5 @@ app.get('/', (req,res)=>{
 
 
 app.listen(PORT, ()=>{
-    console.log(`listening on port: ${PORT}`)
+    console.log(`Shhhhh, trying to read on port: ${PORT}`)
 })
